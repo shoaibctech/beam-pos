725744af7fb7d4298d481f7f56b7fd8996372229
Shoaib <shoaiby.akbar@gmail.com>
2020-07-15 03:43:57 +0500

    added tabs

--- src/container/Transactions/index.js
+++ src/container/Transactions/index.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Modal from 'react-modal';
 import Loader from "react-loader-spinner";
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { getUserData, makeSecureRequest } from "../../utils";
 
 import './styles.css';
@@ -17,6 +22,50 @@ const customStyles = {
         transform             : 'translate(-50%, -50%)'
     }
 };
+
+const useStyles = makeStyles({
+    root: {
+        color: '#09f4c8',
+    },
+    indicator: {
+        color: '#09f4c8'
+    }
+});
+
+const refundTestData = [
+    {
+        id: "azo4kal13b",
+        uri: "/payments/ybo8zayk2q/refunds/azo4kal13b",
+        creationDateTime: "2019-02-28T10:43:07.00Z",
+        lastUpdateDateTime: "2019-02-28T10:43:07.00Z",
+        status: "REFUND_PENDING",
+        refundAmount: 1,
+        compensationAmount: .01,
+        currency: "GBP",
+        remittanceInformation: {
+            reference: "reference",
+            unstructured: "unstructuredRemittanceInfo"
+        },
+        endToEndIdentification: "endToEndIdentification",
+        errorDetails: null
+    },
+    {
+        id: "azo4kal13b",
+        uri: "/payments/ybo8zayk2q/refunds/azo4kal13b",
+        creationDateTime: "2019-02-28T10:43:07.00Z",
+        lastUpdateDateTime: "2019-02-28T10:43:07.00Z",
+        status: "REFUND_COMPLETED",
+        refundAmount: 1,
+        compensationAmount: .01,
+        currency: "GBP",
+        remittanceInformation: {
+            reference: "reference",
+            unstructured: "unstructuredRemittanceInfo"
+        },
+        endToEndIdentification: "endToEndIdentification",
+        errorDetails: null
+    }
+];
 const Transactions = () => {
     const [paymentList, setPaymentsList] = useState([]);
     const [transError, setTransError] = useState('');
@@ -30,6 +79,14 @@ const Transactions = () => {
     const [refundError, setRefundError] = useState('');
     const [balance, setBalance] = useState([]);
     const [balanceError, setBalanceError] = useState('');
+    const [tabValue, setTabValue] = React.useState(0);
+    const [refundedValue, setRefundedValue] = useState();
+    const [refundList, setRefundList] = useState([]);
+    const [isFetchingRefundList, setIsFetchingRefundList] = useState(false);
+    const [refundLimitError, setRefundLimitError] = useState(false);
+
+
+    const classes = useStyles();
 
     useEffect( () => {
         getPaymentsList();
@@ -54,6 +111,41 @@ const Transactions = () => {
 
     }
 
+    const getCreditTransferList = async (merchantId) => {
+        setIsFetching(true);
+        setTransError('');
+        try {
+            const paymentList = await makeSecureRequest(`${process.env.REACT_APP_BACKEND_URL}/api/payments/credit_transfer`,
+                {merchantId: merchantId}, 'POST' );
+
+            console.log('ccccccccccccccccccccccccc', paymentList.data.paymentList)
+            setIsFetching(false);
+            setPaymentsList( prevState => ({...prevState, ...paymentList.data.paymentList}));
+        } catch (e) {
+            console.log('transssssssssssssssssssssss ', e);
+            setIsFetching(false);
+            setTransError('With draw transaction list request failed...')
+        }
+
+    }
+
+    const getRefundPaymentsList = async () => {
+        setIsFetching(true);
+        setTransError('');
+        try {
+            const paymentList = await makeSecureRequest(`${process.env.REACT_APP_BACKEND_URL}/api/payments`,
+                {email: getUserData().email}, 'POST' );
+
+            console.log('ffffffffffffffffffffffff', paymentList.data.paymentList)
+            setIsFetching(false);
+            setPaymentsList( prevState => ({...prevState, ...paymentList.data.paymentList}));
+        } catch (e) {
+            console.log('transssssssssssssssssssssss ', e);
+            setIsFetching(false);
+            setTransError('Refund transaction request failed...')
+        }
+    }
+
     const getBalance = async () => {
         setBalanceError('');
         try{
@@ -66,23 +158,29 @@ const Transactions = () => {
     }
 
     const sureRefund = (index) => {
+        console.log('going to get list of refund amount ')
         setIsOpen(true);
         setRefundIndex(index);
         setRefundPayment( prevState => ({...prevState, ...paymentList.data[index]}));
+        getRefundList(index);
     }
     const refund = async () => {
+
+        if(amount > (refundPayment.amount - refundedValue)){
+            setRefundLimitError(true);
+        }
         console.log('going to refund', refundPayment);
         setIsRefunding(true);
         setRefundError('');
         try {
-            const refundStatus = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/payment/refund`, {
+            const refundStatus = await makeSecureRequest(`${process.env.REACT_APP_BACKEND_URL}/api/payment/refund`, {
                 paymentId: paymentList.data[refundIndex].id,
                 amount: refundType === 'full' ? paymentList.data[refundIndex].amount : amount,
                 currency: paymentList.data[refundIndex].currency,
                 identification: paymentList.data[refundIndex].endToEndIdentification,
                 remittanceInformation: paymentList.data[refundIndex].remittanceInformation,
-
-            });
+                merchantId: getUserData().merchant_id,
+            }, 'POST');
             setIsRefunding(false);
             console.log('refund status :', refundStatus);
         } catch (e) {
@@ -96,6 +194,8 @@ const Transactions = () => {
         setRefundType('');
         setIsRefunding(false);
         setRefundError('');
+        setRefundList([]);
+        setRefundLimitError(false)
     }
     const renderTable = (data) => {
         return data.map( (payment, idx) => {
@@ -114,26 +214,111 @@ const Transactions = () => {
         });
     }
 
-    console.log(transError)
-    console.log(!transError)
+    const renderRefundTable = (data) => {
+        return data.map( (payment, idx) => {
+            return (   <tr key={idx}>
+                <td>{idx}</td>
+                <td>{payment.refundAmount}</td>
+                <td>{payment.currency}</td>
+                <td>{payment.status}</td>
+            </tr>);
+        });
+    }
+    const createCreditTransfer = async () => {
+        try {
+            console.log('creating credit transfer ');
+            const req = await makeSecureRequest(`${process.env.REACT_APP_BACKEND_URL}/api/credit/transfer`, {
+                amount: 100,
+                currency: 'GBP',
+                merchantId: getUserData().merchant_id,
+            }, 'POST');
 
+            console.log('data ::', req.data.data);
+
+        } catch (e) {
+            console.log("error ::", e.response)
+        }
+    }
+
+    const handleTabChange = (event, newValue) => {
+        setTabValue(newValue);
+        if(newValue === 0){
+            getPaymentsList();
+        } else if(newValue === 1) {
+            getCreditTransferList(getUserData().merchant_id);
+            console.log('new Value', newValue);
+        }
+    };
+
+    const getRefundList = async (index) => {
+        try {
+            setIsFetchingRefundList(true);
+            const refundData = await makeSecureRequest(`${process.env.REACT_APP_BACKEND_URL}/api/payment/refund-list`, {
+                paymentId: paymentList.data[index].id,
+                merchantId: getUserData().merchant_id
+            }, 'POST');
+
+            console.log('refund data ::', refundData.data.data);
+            if (refundData.data.data.length > 0) {
+                console.log('refund list')
+                setRefundType('partial');
+                setRefundList( prevState => ([...prevState, ...refundData.data]));
+                calculateRefundAbleAmount(refundData.data);
+            } else {
+                setRefundType('partial');
+                console.log('refun test data ', refundTestData)
+                setRefundList( prevState => ([...prevState, ...refundTestData]));
+                calculateRefundAbleAmount(refundTestData);
+            }
+            setIsFetchingRefundList(false);
+        } catch (e) {
+            setIsFetchingRefundList(false);
+            console.log('errors ::', e);
+        }
+    }
+    const calculateRefundAbleAmount = (data) => {
+        let amount = 0;
+        data.map( x => {
+            if (x.status === 'REFUND_COMPLETED' || x.status === 'REFUND_PENDING'){
+                amount = amount + x.refundAmount;
+            }
+        });
+        console.log('refunded ::', amount);
+        setRefundedValue(amount);
+    }
+
+    console.log('data refund list::', refundList)
     return (
         <div className="transaction">
-            <div className="balance-con">
-                <div className="balance-sec">
-                    {
-                        balanceError ?
-                            <p className="t-error">{balanceError}</p>
-                            :
-                            <p>
-                                {balance && balance.length > 0 ?
-                                    <span className="balance">{balance[1].balance.amount} { balance[1].balance.currency}</span>:
-                                    "loading..."}
-                            </p>
-                    }
-                    <p>BALANCE</p>
-                </div>
-            </div>
+           <div className="tabs-balance">
+               <Paper square >
+                   <Tabs
+                       value={tabValue}
+                       indicatorColor="primary"
+                       textColor="primary"
+                       onChange={handleTabChange}
+                       aria-label="disabled tabs example"
+                   >
+                       <Tab label="Payments" />
+                       <Tab label="With Draws" />
+                   </Tabs>
+               </Paper>
+               <div className="balance-con">
+                   <div className="balance-sec">
+                       <p className="balance-label">BALANCE: {' '}</p>
+                       {
+                           balanceError ?
+                               <p className="t-error">{balanceError}</p>
+                               :
+                               <p>
+                                   {balance && balance.length > 0 ?
+                                       <span className="balance">{balance[1].balance.amount} { balance[1].balance.currency}</span>:
+                                       "loading..."}
+                               </p>
+                       }
+                   </div>
+               </div>
+           </div>
             <h2 className="heading">Transactions Details</h2>
             <table>
                 <thead>
@@ -173,35 +358,68 @@ const Transactions = () => {
                 contentLabel="Example Modal"
             >
                 <div className="modal-container">
+                    {
+                        refundList && refundList.length > 0 ?
+                            <div>
+                                <h3>Refund Transactions </h3>
+                                <table>
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Amount</th>
+                                        <th>Currency</th>
+                                        <th>Status</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {/*{ !isFetchingRefundList ? refundList && refundList.length ? renderRefundTable(refundList)*/}
+                                    {/*    : <tr rowSpan="4" style={{height: '10rem'}}>*/}
+                                    {/*        <td colSpan="8" className="loading">No data found...</td>*/}
+                                    {/*    </tr>*/}
+                                    {/*    : <tr rowSpan="4" style={{height: '10rem'}}>*/}
+                                    {/*        <td colSpan="8" className="loading">Loading...</td>*/}
+                                    {/*    </tr>*/}
+                                    {/*}*/}
+                                    {refundList && refundList.length && renderRefundTable(refundList)}
+                                    </tbody>
+                                </table>
+                            </div>
+                       :
+                            ''
+                    }
                     <h2>Refunding</h2>
                     {
                         !isRefunding &&
                         <div>
+                            { !isFetchingRefundList && <p>Maximum refund amount for this transaction: {(refundPayment.amount - refundedValue).toFixed(2)}</p>}
                             <div>
                                 <div>
-                                    <div>
-                                        Refund type:
-                                        <label className="refund-radio-label">
-                                            <input
-                                                className="refund-radio-btn"
-                                                type="radio"
-                                                value="full"
-                                                checked={refundType === 'full'}
-                                                onChange={ ()=> setRefundType('full')}
-                                            />
-                                            Full
-                                        </label>
-                                        <label className="refund-radio-label">
-                                            <input
-                                                className="refund-radio-btn"
-                                                type="radio"
-                                                value="partial"
-                                                checked={refundType === 'partial'}
-                                                onChange={ ()=> setRefundType('partial')}
-                                            />
-                                            Partial
-                                        </label>
-                                    </div>
+                                    {
+                                        refundList && refundList.length < 0 &&
+                                        <div>
+                                            Refund type:
+                                            <label className="refund-radio-label">
+                                                <input
+                                                    className="refund-radio-btn"
+                                                    type="radio"
+                                                    value="full"
+                                                    checked={refundType === 'full'}
+                                                    onChange={ ()=> setRefundType('full')}
+                                                />
+                                                Full
+                                            </label>
+                                            <label className="refund-radio-label">
+                                                <input
+                                                    className="refund-radio-btn"
+                                                    type="radio"
+                                                    value="partial"
+                                                    checked={refundType === 'partial'}
+                                                    onChange={ ()=> setRefundType('partial')}
+                                                />
+                                                Partial
+                                            </label>
+                                        </div>
+                                    }
                                 </div>
                             </div>
                             {
@@ -214,13 +432,19 @@ const Transactions = () => {
                                     <input
                                         placeholder="Enter amount"
                                         type="number"
-                                        onChange={(e) => setAmount(e.target.value)}
+                                        onChange={(e) => {
+                                            setAmount(e.target.value);
+                                            setRefundLimitError(false);
+                                        }}
                                         value={amount}/>
                                 </div>
                             }
                             {
                                 refundType === 'partial' && amount !== '' &&
                                     <p>Are you sure you want to refund {amount} {refundPayment.currency}?</p>
+                            }
+                            {
+                                refundLimitError && <p className="error_text">Amount exceeded from maximum allowed amount.</p>
                             }
                             <br/>
                             <div className="modal-footer">
