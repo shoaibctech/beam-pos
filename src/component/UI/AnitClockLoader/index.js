793214a9d723e357f9f53a695f913cc52525281b
Muhammad Farooq <farooq@c-tech.io>
2020-12-11 00:38:13 +0500

    fix loader and change text

--- src/component/UI/AnitClockLoader/index.js
+++ src/component/UI/AnitClockLoader/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './styles.css';
 
-export default ({ message = 'loading...', color = 'black', height = 120, width = 120 }) => {
+export default ({ message = 'loading...', color = 'black', height = 50, width = 50 }) => {
   return (
      <div>
-       <div className="anti-clock-loader" style={{borderTop: `9px solid ${color}`, height: height, width: width}}></div>
+       <div className="anti-clock-loader" style={{borderTop: `4px solid ${color}`, height: height, width: width}}></div>
        <h3 className="anti-clock-message">{message}</h3>
      </div>
   );
